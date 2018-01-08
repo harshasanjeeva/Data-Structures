273ed909b36c6404aa26a54d555aeed334775a77
harshasanjeeva <harshasanjiva@gmail.com>
2018-01-08 15:31:06 -0800

    data structures

---
+++ src/linkedlist.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* next;
+};
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
